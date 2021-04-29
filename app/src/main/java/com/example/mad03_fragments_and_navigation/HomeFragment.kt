d85bd75636a0b993084c7fe2dea0672914588e55
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2021-04-29 21:10:49 +0200

    Initial

--- app/src/main/java/com/example/mad03_fragments_and_navigation/HomeFragment.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/HomeFragment.kt
@@ -2,10 +2,9 @@ package com.example.mad03_fragments_and_navigation
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.mad03_fragments_and_navigation.adapters.MovieListAdapter
 import com.example.mad03_fragments_and_navigation.databinding.FragmentHomeBinding
