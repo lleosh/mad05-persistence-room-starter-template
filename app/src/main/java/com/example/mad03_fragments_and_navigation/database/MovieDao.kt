cc44648a67ffe161dc489d5b26df467b4f64fa3a
Leonita Shala <leonita.shala@stud.fh-campuswien.ac.at>
2021-05-23 23:41:06 +0200

    ld5 updated

--- app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/database/MovieDao.kt
@@ -1,5 +1,20 @@
 package com.example.mad03_fragments_and_navigation.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mad03_fragments_and_navigation.models.Movie
+
 interface MovieDao {
     // TODO implement me
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun create(movie: Movie): Long
+
+    @Update
+    suspend fun update(movie: Movie)
+
+    @Query("SELECT * FROM movie_table")
+    fun getAll(): LiveData<List<Movie>>
 }
